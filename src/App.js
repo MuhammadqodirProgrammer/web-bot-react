e5af245201d7000016f05efb3811e7538110cd5b
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2023-10-02 16:29:53 +0500

    add clinick

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import Header from "./components/Header/Header";
 import { Outlet, Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
-<<<<<<< HEAD
 import SingleClinic from "./pages/SingleClinic/SingleClinic";
 import Clinic from "./pages/Clinic/Clinic";
-=======
-import SingleClinic from './pages/SingleClinic/SingleClinic';
-import Admin from './pages/Admin/Admin';
->>>>>>> 5a1c91e05c0feea2ef683f2408bdd8c4d8b586a0
+import Admin from "./pages/Admin/Admin";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -20,7 +16,6 @@ function App() {
     tg.ready();
   }, []);
 
-<<<<<<< HEAD
   return (
     <div className="App w-[100%] px-[10px] ">
       <Header />
@@ -28,25 +23,12 @@ function App() {
         <Route path="/" element={<ProductList />} />
         <Route path="clinic/:id" element={<SingleClinic />} />
         <Route path="clinic" element={<Clinic />} />
+        <Route path={"admin"} element={<Admin />} />
+
         <Route path={"form"} element={<Form />} />
       </Routes>
     </div>
   );
-=======
-    return (
-        <div className="App w-[100%] px-[10px] ">
-            <Header />
-            <Routes>
-                <Route path='/' element={<ProductList />}/>
-                 <Route path='clinic/:id' element={<SingleClinic/>} />
-
-
-                <Route path={'form'} element={<Form />}/>
-                <Route path={'admin'} element={<Admin />}/>
-            </Routes>
-        </div>
-    );
->>>>>>> 5a1c91e05c0feea2ef683f2408bdd8c4d8b586a0
 }
 
 export default App;
